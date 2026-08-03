7c4dc733fc4e943f15d42bfb5f5be8ee1a8b9e5b
ansofhn <ansofhn5@gmail.com>
2026-08-03 14:10:24 +0700

    fix: update db

--- config/db.php
+++ config/db.php
@@ -6,7 +6,7 @@ $DB_USER = getenv('MYSQLUSER')     ?: 'root';
 $DB_PASS = getenv('MYSQLPASSWORD') ?: '';
 $DB_NAME = getenv('MYSQLDATABASE') ?: 'tmasch_db';
 
-$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, (int)$DB_PORT);
+$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
 
 if ($conn->connect_error) {
     die("Koneksi database gagal: " . $conn->connect_error);
